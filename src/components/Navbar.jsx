b913c4200b97b52d21a72b10ef90edb13923892c
Emkei <kabochakiongo@gmail.com>
2025-06-28 01:58:38 +0300

    navbar-refine

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { FaArrowRightLong, FaHandHoldingHeart } from "react-icons/fa6";
+import { MdOutlineCloseFullscreen } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 function Navbar() {
   const [isClicked, setIsClicked] = useState(false);
 
   function toggleMenu() {
-    setIsClicked(prev => !prev);
+    setIsClicked((prev) => !prev);
   }
 
   return (
     <div>
       {/* Top contact bar */}
-      <div className="h-10 w-full bg-gradient-to-r from-blue-300 to-blue-600 flex justify-end">
+      <div className="h-10 w-full bg-gradient-to-r from-blue-300 to-blue-600 md:flex hidden justify-end">
         <h1 className="text-white mr-4 items-center">+254769212169</h1>
       </div>
 
       {/* Main navbar */}
       <div className="flex items-center h-30 bg-blue-300 justify-between px-4">
-        {/* Logo section */}
-        <div className="hover:bg-blue-200">
-          <h1 className="text-blue-500">Waves</h1>
-          {/* Example logo placeholder */}
-          <img src="/logo.png" alt="logo" className="h-8" />
+        {/* Mobile Menu Button */}
+        <div className="flex items-center gap-2">
+          <button
+            onClick={toggleMenu}
+            className="text-black text-2xl  md:hidden"
+          >
+            {isClicked ? <MdOutlineCloseFullscreen /> : <RxHamburgerMenu />}
+          </button>
+          <h1 className="text-white  text-3xl font-sans font-bold">WAVES</h1>
         </div>
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex mr-4">
-          {['Home', 'About', 'Programs', 'Donate'].map((item) => (
+          {[
+            "Home",
+            "About us",
+            "Our Programs",
+            "Our Team",
+            "Get Involved",
+            "Contact Us",
+          ].map((item) => (
             <li
               key={item}
               className="text-white relative m-4 after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[3px] after:w-0 after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full"
@@ -34,30 +48,36 @@ function Navbar() {
             </li>
           ))}
         </ul>
-
-        {/* Mobile Menu Button */}
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-white text-3xl focus:outline-none border-1"
-          >
-            {isClicked ? 'X' : '='}
+        <div>
+          <button className="flex items-center gap-2 bg-blue-500 text-white m-2 p-2 rounded-b-xl ">
+            <FaHandHoldingHeart />
+            Donate <span className="hidden md:flex">to our cause</span>
+            <FaArrowRightLong />
           </button>
         </div>
       </div>
 
       {/* Mobile Dropdown Menu */}
       {isClicked && (
-        <ul className="md:hidden flex flex-col bg-blue-500 px-4 py-2 space-y-2">
-          {['Home', 'About', 'Programs', 'Donate'].map((item) => (
-            <li
-              key={item}
-              className="text-white relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[3px] after:w-0 after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full"
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
+        <nav className="relative md:hidden flex flex-col h-screen bg-white  ">
+          <ul className=" px-4 py-2 space-y-2 text-black">
+            {[
+              "Home",
+              "About us",
+              "Our Programs",
+              "Our Team",
+              "Get Involved",
+              "Contact Us",
+            ].map((item) => (
+              <li
+                key={item}
+                className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[3px] after:w-0 after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-25"
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </nav>
       )}
     </div>
   );
