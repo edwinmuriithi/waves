13f3b652509eda02527881fb5d9fe3440538eaf4
mark abwoga <markabwoga@gmail.com>
2025-06-28 04:49:32 +0300

    feat(images): modified hero section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import HeroSection from './HeroSection'
 
 function HomePage() {
   return (
-    <div><Navbar /></div>
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
   )
 }
 
